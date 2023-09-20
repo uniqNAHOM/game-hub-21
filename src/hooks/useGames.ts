99f0b34118c9cae9edc8b00ae1a491f0a0cc861a
nahom <nahomaseged2018@gmail.com>
2023-09-19 20:50:46 -0700

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,7 +20,8 @@ const useGames = (
    {
     params:{
       genres : gameQuery.genre?.id,
-      platforms : gameQuery.platform?.id
+      platforms : gameQuery.platform?.id,
+      ordering : gameQuery.sortOrder
       }
     },
     [gameQuery])
