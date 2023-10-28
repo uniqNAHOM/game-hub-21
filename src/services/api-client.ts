5a72b89059aeb6de1771617ca8c8177eea89f2b1
uniqNAHOM <130177276+uniqNAHOM@users.noreply.github.com>
2023-10-28 12:34:33 +0300

    Update api-client.ts
    Key change

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios"
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params:{
-        key:'e776fc7c7f6244ff9c754dc574145258'
+        key:''
     }
 })
